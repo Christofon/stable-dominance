064cc0c2f1dd0786527988f94e4ea5c6102cc238
simogab <56610676+simogab@users.noreply.github.com>
2020-09-09 14:10:59 +0200

    csv

--- backend/history.py
+++ backend/history.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 from pycoingecko import CoinGeckoAPI
-cg = CoinGeckoAPI()
 
+cg = CoinGeckoAPI()
 coins_list = cg.get_coins_list() #to extract relevant ids
 
 tether = cg.get_coin_market_chart_range_by_id('tether','usd',1426377600,1599523200) #tether data 15.3.2015-8.9.2020
@@ -11,14 +11,11 @@ tether_unix = list(range(1427673600,1599523200,86400)) #adjusted lengths by 15
 tether_mcap_pd = pd.DataFrame({'unix':tether_unix,'tether_mcap':tether_mcap})
 tether_mcap_pd.to_csv('tether_mcap.csv',sep=',')
 
-
-
 usdc = cg.get_coin_market_chart_range_by_id('usd-coin','usd',1539129600,1599523200) #usdc data -8.9.2020
 usdc_mcap = np.array(usdc['market_caps'])[:,1]
 usdc_unix = list(range(1539043200,1599523200,86400)) #offset 1x86400
 usdc_mcap_pd = pd.DataFrame({'unix':usdc_unix,'usdc_mcap':usdc_mcap})
 usdc_mcap_pd.to_csv('usdc_mcap.csv',sep=',')
-
 
 dai = cg.get_coin_market_chart_range_by_id('dai','usd',1574380800,1599523200) #dai data -8.9.2020
 dai_mcap = np.array(dai['market_caps'])[:,1]
