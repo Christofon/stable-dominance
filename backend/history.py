ba63b7698a3bfdb191e6a9c1d2d9b61d4f6f68c7
simogab <56610676+simogab@users.noreply.github.com>
2020-09-10 20:33:07 +0200

    removed index
    .csv files now contain only unix timestamp & marketcap

--- backend/history.py
+++ backend/history.py
@@ -8,37 +8,37 @@ tether = cg.get_coin_market_chart_range_by_id('tether','usd',1426377600,15995232
 tether_mcap = np.array(tether['market_caps'])[:,1]
 tether_unix = list(range(1427673600,1599523200,86400)) #offset 15x86400
 tether_mcap_pd = pd.DataFrame({'unix':tether_unix,'tether_mcap':tether_mcap})
-tether_mcap_pd.to_csv('tether_mcap.csv',sep=',')
+tether_mcap_pd.to_csv('tether_mcap.csv',sep=',',index=False)
 
 tusd = cg.get_coin_market_chart_range_by_id('true-usd','usd',1521936000,1599523200) #tusd data 25.3.2018-8.9.2020
 tusd_mcap = np.array(tusd['market_caps'])[:,1]
 tusd_unix = list(range(1521849600,1599523200,86400)) #offset 1x86400
 tusd_mcap_pd = pd.DataFrame({'unix':tusd_unix,'tusd_mcap':tusd_mcap})
-tusd_mcap_pd.to_csv('tusd_mcap.csv',sep=',')
+tusd_mcap_pd.to_csv('tusd_mcap.csv',sep=',',index=False)
 
 susd = cg.get_coin_market_chart_range_by_id('nusd','usd',1534204800,1599523200) #susd data 14.8.2018 -8.9.2020
 susd_mcap = np.array(susd['market_caps'])[:,1]
 susd_unix = list(range(1534204800,1599523200,86400))
 susd_mcap_pd = pd.DataFrame({'unix':susd_unix,'susd_mcap':susd_mcap})
-susd_mcap_pd.to_csv('susd_mcap.csv',sep=',')
+susd_mcap_pd.to_csv('susd_mcap.csv',sep=',',index=False)
 
 pax = cg.get_coin_market_chart_range_by_id('paxos-standard','usd',1538006400,1599523200) #paxos-standard data 27.9.2018-8.9.2020
 pax_mcap = np.array(pax['market_caps'])[:,1]
 pax_unix = list(range(1537920000,1599523200,86400)) #offset 1x86400
 pax_mcap_pd = pd.DataFrame({'unix':pax_unix,'pax_mcap':pax_mcap})
-pax_mcap_pd.to_csv('pax_mcap.csv',sep=',')
+pax_mcap_pd.to_csv('pax_mcap.csv',sep=',',index=False)
 
 usdc = cg.get_coin_market_chart_range_by_id('usd-coin','usd',1539129600,1599523200) #usdc data 10.10.2018-8.9.2020
 usdc_mcap = np.array(usdc['market_caps'])[:,1]
 usdc_unix = list(range(1539043200,1599523200,86400)) #offset 1x86400
 usdc_mcap_pd = pd.DataFrame({'unix':usdc_unix,'usdc_mcap':usdc_mcap})
-usdc_mcap_pd.to_csv('usdc_mcap.csv',sep=',')
+usdc_mcap_pd.to_csv('usdc_mcap.csv',sep=',',index=False)
 
 dai = cg.get_coin_market_chart_range_by_id('dai','usd',1574380800,1599523200) #dai data 22.11.2019-8.9.2020
 dai_mcap = np.array(dai['market_caps'])[:,1]
 dai_unix = list(range(1574294400,1599523200,86400)) #offset 1x86400
 dai_mcap_pd = pd.DataFrame({'unix':dai_unix,'dai_mcap':dai_mcap})
-dai_mcap_pd.to_csv('dai_mcap.csv',sep=',')
+dai_mcap_pd.to_csv('dai_mcap.csv',sep=',',index=False)
 
 #to add - busd, husd, mUSD, USDK, gUSD, SAI, EOSDT
 
@@ -66,13 +66,9 @@ def total(): #calculate sum of marketcaps
 
 total_mcap = np.array(total())
 total_mcap_pd = pd.DataFrame({'unix':tether_unix,'total_mcap':total_mcap})
-total_mcap_pd.to_csv('total_mcap.csv',sep=',')
+total_mcap_pd.to_csv('total_mcap.csv',sep=',',index=False)
 
 #Tether dominance calculater - change to functional if mcap is greatest
 dominance = (tether_mcap/total_mcap)*100 
 dominance_pd = pd.DataFrame({'unix':tether_unix,'tether_dominance':dominance})
-dominance_pd.to_csv('tether_dominance.csv',sep=',')
-
-#add stable coin protocol dominance - ETH/omni/EOS etc.
-#add EUR/CHF/CAD
-#add BTC protocol dominance vs. layer2 lighting/liquid/renbtc/wbtc
+dominance_pd.to_csv('tether_dominance.csv',sep=',',index=False)
--- backend/updater.py
+++ backend/updater.py
@@ -10,8 +10,13 @@ tether = pd.read_csv('tether_mcap.csv') #read old data
 tether_lastrow = np.array(tether.iloc[-1:]) #get last row
 print(int(tether_lastrow[:,1])) #[:,1] #0 = counter, 1 = unix, 2 = mcap
 
+
 #get newest data in usable format
 tether = cg.get_coin_market_chart_range_by_id(id='tether',vs_currency='usd',from_timestamp=int(tether_lastrow[:,1]),to_timestamp=now) 
-
+tether_mcap = np.array(tether['market_caps'])[:,1]
+tether_unix = list(range(int(tether_lastrow[:,1]),now,int((now-int(tether_lastrow[:,1]))/len(tether_mcap)+1)))
+print(len(tether_mcap),len(tether_unix))
+tether_mcap_pd = pd.DataFrame({'unix':tether_unix,'tether_mcap':tether_mcap})
+print(tether_mcap_pd)
 
 #save to .csv
