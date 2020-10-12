29cfd3baec1216f7bf691b9a5ea52f5ac32a9561
Christofon <kremsnerchristopher@gmail.com>
2020-10-12 19:17:09 +0200

    add footer and social buttons

---
+++ src/App/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+const Bar = styled.div`
+  margin-top: 120px;
+`;
+
+const SocialButtons = styled.div`
+  text-align: center;
+`;
+
+const Link = styled.a`
+  margin: 20px;
+`;
+
+export default function () {
+  return (
+    <Bar>
+      <SocialButtons>
+        <Link href="https://twitter.com/stabledominance" target="_blank">Twitter</Link>
+        <Link href="https://medium.com/stabledominance" target="_blank">Medium</Link>
+      </SocialButtons>
+    </Bar>
+  );
+}
--- src/App/index.js
+++ src/App/index.js
@@ -5,6 +5,7 @@ import AppBar from "./AppBar";
 import { AppProvider } from "./AppProvider";
 import Dashboard from "../Dashboard";
 import Content from "../Shared/Content";
+import Footer from "./Footer"
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
           <Content>
             <Dashboard />
           </Content>
+          <Footer />
         </AppProvider>
       </AppLayout>
     );
